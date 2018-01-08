2a8c9fa3cd2dcb9d7e5d5ea4fe7383efa9589187
zhouhao <zhouhao@jiandan100.cn>
2018-01-08 17:03:12 +0800

    图片预览

---
+++ demo01-cookie/demo01.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zh on 2018/1/5.
+ */
+var url = window.url;
+var index = url.indexOf('callBack');
+var callBack = url.slice(index + 'callBack='.length, url.length);
+eval(callBack)({name : 'jay'});
+// setTimeout(callBack + "({name : 'jay'})", 0);
