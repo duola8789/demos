5898148b1744c4c9a7d0a425103dabf4af35cd3e
zh <duola8789@126.com>
2020-05-23 09:52:29 +0800

    feat: 深入浅出Vue-watch

--- demo44-vue/reactive-object.js
+++ demo44-vue/reactive-object.js
@@ -1,6 +1,8 @@
 /**
  * Created by zh on 2020/4/2.
  */
+const isObject = (val) => Object.prototype.toString.call(val) === '[object Object]';
+
 const parsePath = (path) => {
   const segments = path.split('.');
   return function (obj) {
@@ -38,6 +40,7 @@ const defineReactive = (data, key, value) => {
 
 class Dep {
   constructor() {
+    this.id = uid++;
     this.subs = [];
   }
 
@@ -49,13 +52,15 @@ class Dep {
     if (this.subs.length > 0) {
       const index = this.subs.indexOf(sub);
       if (index > -1) {
+        return this.subs.splice(index, 1);
       }
     }
   }
 
   depend() {
     if (window.target) {
-      this.addSub(window.target);
+      // this.addSub(window.target);
+      window.target.addDep(this);
     }
   }
 
@@ -68,16 +73,38 @@ class Dep {
 }
 
 class Watcher {
-  constructor(vm, expOrFn, cb) {
+  constructor(vm, expOrFn, cb, options) {
     this.vm = vm;
     this.cb = cb;
-    this.getter = parsePath(expOrFn);
+
+    if (options) {
+      this.deep = !!options.deep;
+    } else {
+      this.deep = false;
+    }
+
+    // 记录自己订阅的依赖列表
+    this.deps = [];
+    // 依赖列表的 Id
+    this.depIds = new Set();
+
+    // expOrFn 支持函数
+    if (typeof expOrFn === 'function') {
+      this.getter = expOrFn;
+    } else {
+      this.getter = parsePath(expOrFn);
+    }
     this.value = this.get();
   }
 
   get() {
     window.target = this;
     this.value = this.getter.call(this.vm, this.vm);
+    // deep 逻辑
+    if (this.deep) {
+      traverse(this.value);
+    }
+
     window.target = undefined;
     return this.value;
   }
@@ -86,6 +113,24 @@ class Watcher {
     const oldValue = this.value;
     this.value = this.get();
     this.cb.call(this.vm, this.value, oldValue);
+  }
+
+  addDep(dep) {
+    const id = dep.id;
+    if (!this.depIds.has(id)) {
+      this.depIds.add(id);
+      this.deps.push(dep);
+      // 将自己（watcher实例）添加的 dep 的 subs 列表中
+      dep.addSub(this);
+    }
+  }
+
+  //  从所有依赖项的 Dep 列表中把自己移除
+  teardown() {
+    let i = this.deps.length;
+    while (i--) {
+      this.deps[i].removeSub(this);
+    }
   }
 }
 
@@ -113,3 +158,20 @@ const watcher = new Watcher(data, 'a', (newVal, oldVal) => {
   console.log('watcher', newVal, oldVal, '999');
 });
 console.log(data.a);
+
+Vue.prototype.$watch = function (expOrFn, cb, options) {
+  const vm = this;
+  options = options || {};
+
+  const watcher = new Watcher(vm, expOrFn, cb, options);
+
+  if (options.immediate) {
+    cb.call(vm, watcher.value);
+  }
+
+  return function unwatch() {
+    watcher.teardown();
+  };
+};
+
+const seenObjects = new Set();
