d2bc957fee974d507dd0f7865662a0c151281239
zh <duola8789@126.com>
2020-10-21 13:52:10 +0800

    feat: 添加demo

--- demo00/temp.js
+++ demo00/temp.js
@@ -1,20 +1,3 @@
 /**
  * Created by zh on 2020/6/24.
  */
-let a = {
-  x: {
-    y: 123
-  }
-};
-
-const proxy = (target, sourceKey) => {
-  return new Proxy(target, {
-    get(obj, key) {
-      return obj[sourceKey][key];
-    }
-  });
-};
-
-const proxyA = proxy(a, 'x');
-
-console.log(proxyA.y); // 123
