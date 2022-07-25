390f0a6ca7cb9e84bbcc0957cd677520b8ac9b9d
duola8789 <duola8789@126.com>
2022-07-25 12:08:11 +0800

    New

--- demo44-深入浅出Vue/extend.js
+++ demo44-深入浅出Vue/extend.js
@@ -4,35 +4,35 @@
 let cid = 1;
 
 Vue.extend = function (extendOptions) {
-  extendOptions = extendOptions || {};
+    extendOptions = extendOptions || {};
 
-  const Super = this;
-  const SuperId = Super.cid;
-  const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {});
+    const Super = this;
+    const SuperId = Super.cid;
+    const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {});
 
-  if (cachedCtors[SuperId]) {
-    return cachedCtors[SuperId];
-  }
+    if (cachedCtors[SuperId]) {
+        return cachedCtors[SuperId];
+    }
 
-  const name = extendOptions.name || Super.options.name;
-  if (process.env.NODE_ENV !== 'production') {
-    // 对组件名字进行校验，在开发模式下进行警告
-  }
+    const name = extendOptions.name || Super.options.name;
+    if (process.env.NODE_ENV !== 'production') {
+        // 对组件名字进行校验，在开发模式下进行警告
+    }
 
-  const Sub = function VueComponent(options) {
-    this._init(options);
-  };
+    const Sub = function VueComponent(options) {
+        this._init(options);
+    };
 
-  Sub.prototype = Object.create(Super.prototype);
-  Sub.prototype.constructor = Sub;
-  Sub.cid = cid++;
+    Sub.prototype = Object.create(Super.prototype);
+    Sub.prototype.constructor = Sub;
+    Sub.cid = cid++;
 
-  Sub.options = mergeOptions(Super.options, extendOptions);
-  Sub['super'] = Super;
+    Sub.options = mergeOptions(Super.options, extendOptions);
+    Sub['super'] = Super;
 
-  // 处理 props、computed 等属性
+    // 处理 props、computed 等属性
 
-  // 缓存构造函数
-  cachedCtors[SuperId] = Sub;
-  return Sub;
+    // 缓存构造函数
+    cachedCtors[SuperId] = Sub;
+    return Sub;
 };
