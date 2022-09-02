6667114e371530db572131cb196f6045cd452222
duola8789 <duola8789@126.com>
2022-09-02 10:37:50 +0800

    IndexedDb

--- demo47-indexdb/dexie.js
+++ demo47-indexdb/dexie.js
@@ -3,21 +3,55 @@
  */
 const db = new Dexie('friend_database');
 db.version(6).stores({
-    friends: 'name, [age+isThread]'
+    friends: 'name, [age+size+isThread], weight'
 });
 
-[
-    { name: 'Jay1', age: 1, isThread: 1 },
-    { name: 'Jay2', age: 21, isThread: 0 },
-    { name: 'Jay3', age: 31, isThread: 0 },
-    { name: 'Jay4', age: 41, isThread: 1 },
-    { name: 'Jay5', age: 51, isThread: 1 }
-].forEach(async (v) => {
-    await db.friends.put({ ...v });
-});
+// db.version(8)
+//     .stores({
+//         persons2: 'name, [age+size+isThread], weight'
+//     })
+//     .upgrade((trans) => {
+//         return trans.friends
+//             .toCollection()
+//             .toArray()
+//             .then((newData) => {
+//                 console.time('bulkPut');
+//                 return trans.persons2.bulkPut(newData);
+//             })
+//             .then(() => {
+//                 console.timeEnd('bulkPut'); // 1716.781982421875 ms
+//             });
+//     });
+
+db.version(11)
+    .stores({
+        persons6: 'name, [age+size+isThread], weight'
+    })
+    .upgrade((trans) => {
+        console.time('put');
+        return trans.friends
+            .each((v) => {
+                trans.persons6.put(v);
+            })
+            .then(() => {
+                console.timeEnd('put');
+            });
+    });
+
+// Array.from({ length: 10000 })
+//     .map((v, index) => ({
+//         name: 'Jay' + index,
+//         age: index,
+//         isThread: index % 2 ? 0 : 1,
+//         size: index + 1000,
+//         weight: 10000
+//     }))
+//     .forEach(async (v) => {
+//         await db.friends.put({ ...v });
+//     });
 
 async function show() {
-    return db.friends.where(['age', 'isThread']).between([0, 0], [100, 1], true, true).toArray();
+    return db.friends.where(['age', 'size', 'isThread']).between([21, 2], [21, 2], true, true).toArray();
 }
 
 show().then((v) => {
