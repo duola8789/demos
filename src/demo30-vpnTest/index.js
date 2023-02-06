937c555886696c5f4d55c2d7f3fd869eb242e6e8
duola8789 <duola8789@126.com>
2023-02-06 11:00:32 +0800

    feat：翻墙测试

---
+++ src/demo30-vpnTest/index.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+  canYouClimbWallTask();
+};
+
+function canYouClimbWall() {
+  return new Promise((resolve) => {
+    const img = new Image();
+    let timer = setTimeout(() => {
+      resolve(false);
+    }, 60000);
+    img.onload = () => {
+      resolve(true);
+      clearTimeout(timer);
+      timer = null;
+    };
+    img.onerror = () => {
+      resolve(false);
+      clearTimeout(timer);
+      timer = null;
+    };
+    img.src = 'https://google.com/favicon.ico?' + Date.now();
+  });
+}
+
+function canYouClimbWallTask() {
+  const TIMER_INTERVAL = 1000;
+  const EXECUTE_INTERVAL = 5000;
+  const LOCAL_KEY = 'WallTest';
+  let isExecuting = false;
+  setInterval(() => {
+    const lastExeCute = +localStorage.getItem(LOCAL_KEY);
+    if (lastExeCute) {
+      if (!isExecuting && Date.now() - lastExeCute >= EXECUTE_INTERVAL) {
+        isExecuting = true;
+        canYouClimbWall().then((val) => {
+          isExecuting = false;
+          if (val) {
+            alert('Yes');
+          } else {
+            alert('No');
+          }
+          localStorage.setItem(LOCAL_KEY, Date.now() + '');
+        });
+      }
+    } else {
+      localStorage.setItem(LOCAL_KEY, Date.now() + '');
+    }
+  }, TIMER_INTERVAL);
+}
